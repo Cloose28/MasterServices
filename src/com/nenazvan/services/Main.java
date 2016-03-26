9f7806105f829136285d1a7b62ab293d52b0a372
Art <ashumanov@gmail.com>
2016-03-26 17:49:25 +0300

    Main simplified.

--- src/com/nenazvan/services/Main.java
+++ src/com/nenazvan/services/Main.java
@@ -3,7 +3,6 @@ package com.nenazvan.services;
 public class Main {
     /** The entry point to the program*/
     public static void main(String[] args) {
-        Controller controller = new Controller();
-        controller.startMenu();
+        new Controller().startMenu();
     }
 }
